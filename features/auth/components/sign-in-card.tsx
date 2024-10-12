d3c5d5fa5550bec89c8b73ad4c08c9e5268c5c0f
Achref Mehri <ashrefmehri24@gmail.com>
2024-10-12 19:53:32 +0200

    creating auth screens with form validation

---
+++ features/auth/components/sign-in-card.tsx
@@ -0,0 +1,131 @@
+import * as z from "zod";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+import { DottedSeparator } from "@/components/dotted-separator";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(1,"Please enter your password"),
+});
+
+export const SignInCard = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+const onSubmit =(values:z.infer<typeof formSchema>)=>{
+console.log(values);
+
+}
+
+  return (
+    <Card className="w-full h-full md:w-[487px] border-none shadow-none">
+      <CardHeader className="flex items-center justify-center text-center p-7 ">
+        <CardTitle className="text-2xl">Welcome back!</CardTitle>
+      </CardHeader>
+      <div className="px-7 ">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              name="email"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="Enter email adress"
+                    {...field}
+                  />
+                  </FormControl>
+                  <FormMessage/>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Enter password"
+                    {...field}
+                  />
+                  </FormControl>
+                  <FormMessage/>
+                </FormItem>
+              )}
+            />
+            <Button  disabled={false} size="lg" className="w-full">
+              Login
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7 flex flex-col space-y-4">
+        <Button
+          disabled={false}
+          variant="secondary"
+          size="lg"
+          className="w-full"
+        >
+          <FcGoogle className="mr-2 size-5" />
+          Login with Google
+        </Button>
+        <Button
+          disabled={false}
+          variant="secondary"
+          size="lg"
+          className="w-full"
+        >
+          <FaGithub className="mr-2 size-5" />
+          Login with Github
+        </Button>
+      </CardContent>
+  <div className="px-7">
+    <DottedSeparator/>
+  </div>
+  <CardContent className="p-7 flex items-center justify-center">
+      <p>Don't you have an account ? </p>
+      <Link href="/sign-up">
+     <span className="text-blue-700">&nbsp; Create Account</span>
+      </Link>
+  </CardContent>
+    </Card>
+  );
+};
